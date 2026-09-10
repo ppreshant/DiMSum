41ce440bd7ebe095816cf8de800a5815bc57f273
Prashant Kalvapalle <ppresh.ant@gmail.com>
2026-09-09 18:53:50 -0600

    creating config and sh command for della_pilot test

--- run_dimsum.sh
+++ run_dimsum.sh
@@ -1,6 +1,10 @@
+#!/bin/bash
+
 # writing the command to run dimsum for quick port/test in HPC
+# Note: save the WT sequence to a file and load it 
 
-# save the WT sequence to a file and load it 
+DiMSum --fastqFileDir ../data/ \
+    --experimentDesignPath della_pilot_ez.txt \
+    --wildtypeSequence "$(cat ./della_wt.txt)" \
+    -o ../results/della_pilot_ez
 
-
-DiMSum --fastqFileDir ../data/ --experimentDesignPath experimentDesign.txt --wildtypeSequence "$(cat wildtype.txt)"
