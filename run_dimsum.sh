954342a8eddbf85543fe72324f04c55ea9674bdb
Prashant Kalvapalle <ppresh.ant@gmail.com>
2026-09-09 19:07:46 -0600

    cleaned up configuration into `config/`

--- run_dimsum.sh
+++ run_dimsum.sh
@@ -3,8 +3,11 @@
 # writing the command to run dimsum for quick port/test in HPC
 # Note: save the WT sequence to a file and load it 
 
+output_dir=results/della_pilot_ez
+mkdir -p $output_dir
+
 DiMSum --fastqFileDir ../data/ \
-    --experimentDesignPath della_pilot_ez.txt \
-    --wildtypeSequence "$(cat ./della_wt.txt)" \
-    -o ../results/della_pilot_ez
+    --experimentDesignPath config/della_pilot_ez.txt \
+    --wildtypeSequence "$(cat config/della_wt.txt)" \
+    -o $output_dir
 
